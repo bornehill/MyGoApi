18918d61e85257204f2ae5279b717db942c0ebb7
bornehill <ali.escamilla.navarro@gmail.com>
2019-12-10 16:42:20 -0600

    add params

--- api.go
+++ api.go
@@ -4,6 +4,8 @@ import (
     "log"
 	"net/http"
 	"github.com/gorilla/mux"
+	"fmt"
+	"strconv"
 )
 
 func get(w http.ResponseWriter, r *http.Request) {
@@ -36,6 +38,37 @@ func notFound(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte(`{"message": "not found"}`))
 }
 
+func params(w http.ResponseWriter, r *http.Request) {
+    pathParams := mux.Vars(r)
+    w.Header().Set("Content-Type", "application/json")
+
+    userID := -1
+    var err error
+    if val, ok := pathParams["userID"]; ok {
+        userID, err = strconv.Atoi(val)
+        if err != nil {
+            w.WriteHeader(http.StatusInternalServerError)
+            w.Write([]byte(`{"message": "need a number"}`))
+            return
+        }
+    }
+
+    commentID := -1
+    if val, ok := pathParams["commentID"]; ok {
+        commentID, err = strconv.Atoi(val)
+        if err != nil {
+            w.WriteHeader(http.StatusInternalServerError)
+            w.Write([]byte(`{"message": "need a number"}`))
+            return
+        }
+    }
+
+    query := r.URL.Query()
+    location := query.Get("location")
+
+    w.Write([]byte(fmt.Sprintf(`{"userID": %d, "commentID": %d, "location": "%s" }`, userID, commentID, location)))
+}
+
 func main() {
 	r := mux.NewRouter()
 	api := r.PathPrefix("/api/v1").Subrouter()
@@ -43,6 +76,7 @@ func main() {
     api.HandleFunc("/", post).Methods(http.MethodPost)
     api.HandleFunc("/", put).Methods(http.MethodPut)
     api.HandleFunc("/", delete).Methods(http.MethodDelete)
-    api.HandleFunc("/", notFound)
+	api.HandleFunc("/", notFound)
+	api.HandleFunc("/user/{userID}/comment/{commentID}", params).Methods(http.MethodGet)
     log.Fatal(http.ListenAndServe(":5021", r))
 }
